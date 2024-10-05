c4c8182f4b7ab2daef2e4eb14fa517b8ee2821d1
Sacred3v <sacredevelopment344@gmail.com>
2024-10-05 12:44:57 -0600

    Parcial01 - Add Users controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/v1/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+     [HttpGet("{id}")]// api/v1/users/2
+        public ActionResult<AppUser> GetUsersById(int id)
+        { 
+            var users = _context.Users.Find(id);
+            if (users == null) return NotFound();
+            return users;
+        }
+
+    }
+    
