28db2b51f4f128b43bfc80f5ab62fb7e9c51ef93
Sacred3v <sacredevelopment344@gmail.com>
2024-10-06 14:00:36 -0600

    Parcial01 - Add login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -26,6 +26,25 @@ public class AccountController(DataContext context) : BaseApiController
         return usr;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> LoginAsync(LoginRequest request){
+        var usr = await context.Users.FirstOrDefaultAsync(x => 
+        x.UserName.ToLower() == request.Username.ToLower());
+        
+        if(usr == null) 
+        return Unauthorized("Invalid username or password");
+
+        using var hmac = new HMACSHA512(usr.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
+
+        for(int i = 0; i < computedHash.Length; i++)
+            if(computedHash[i] != usr.PasswordHash[i]) 
+            return Unauthorized("Invalid username or password");
+        
+
+        return usr;
+    }
+
     private async Task<bool> UserExistsAsync(string username) => 
     await context.Users.AnyAsync(u => u.UserName.ToLower() == username.ToLower());
     
