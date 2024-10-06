5d083bfc0deccb68cb7dbf94686381ba7204b39d
Sacred3v <sacredevelopment344@gmail.com>
2024-10-06 12:48:35 -0600

    Parcial01 - Add RegisterAsync

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> RegisterAsync(string username, string password)
+    {
+        using var hmac = new HMACSHA512();
+        var usr = new AppUser{
+            UserName = username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PasswordSalt = hmac.Key,
+        };
+
+        context.Users.Add(usr);
+        await context.SaveChangesAsync();
+        return usr;
+    }
+}
