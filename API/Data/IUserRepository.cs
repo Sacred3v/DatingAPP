5f68a1a21f7fd488ee8b0afa6cfc5612be5392db
Sacred3v <sacredevelopment344@gmail.com>
2024-10-19 21:20:06 -0600

    Parcial02 - Add REPOSITORY PATTERN

---
+++ API/Data/IUserRepository.cs
@@ -0,0 +1,10 @@
+namespace API.Data;
+
+public interface IUserRepository
+{
+    void Update (AppUser user);
+    Task<bool> SaveAllAsync();
+    Task<IEnumerable<AppUser>> GetAllAsync();
+    Task<AppUser?> GetByIdAsync(int id);
+    Task<AppUser?> GetByUsernameAsync(string username);
+}
