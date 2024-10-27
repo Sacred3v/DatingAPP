81965c17adccaf536de8de90ac5250bd96978f94
Sacred3v <sacredevelopment344@gmail.com>
2024-10-27 10:58:07 -0600

    Parcial02 - Add AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,50 +1,40 @@
-namespace API.Controllers;
 using API.Data;
 using API.DataEntities;
+using API.DTos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+namespace API.Controllers;
+
 [Authorize]
 public class UsersController : BaseApiController
-{
-    private readonly IUserRepository _repository;
+    {
+        private readonly IUserRepository _repository;
 
-    public UsersController(IUserRepository repository)
+    public UsersController (UserRepository repository)
     {
         _repository = repository;
     }
-
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetAllAsync()
-    {
-        var users = await _repository.GetAllAsync();
-
-        return Ok(users);
-    }
-
-    [HttpGet("{id:int}")] // api/users/2
-    public async Task<ActionResult<AppUser>> GetByIdAsync(int id)
-    {
-        var user = await _repository.GetByIdAsync(id);
-
-        if (user == null)
+        public async Task<ActionResult<IEnumerable<MemberResponse>>> GetAllAsync()
         {
-            return NotFound();
+            var users = await _repository.GetAllAsync();
+
+            return  Ok(users);
         }
-
-        return user;
-    }
-
-    [HttpGet("{username}")] // api/users/Calamardo
-    public async Task<ActionResult<AppUser>> GetByUsernameAsync(string username)
-    {
-        var user = await _repository.GetByUsernameAsync(username);
-
-        if (user == null)
-        {
-            return NotFound();
+     [HttpGet("{id:int}")]// api/users/2
+        public async  Task<ActionResult<MemberResponse>> GetAllByIdAsync(int id)
+        { 
+            var users = await _repository.GetByIdAsync(id);
+            if (users == null) return NotFound();
+            return users;
         }
-
-        return user;
+    [HttpGet("{username}")]// api/v1/users/2
+        public async  Task<ActionResult<MemberResponse>> GetByIdAsync(string username)
+        { 
+            var user = await _repository.GetByUsernameAsync(username);
+            if (user == null) return NotFound();
+            return user;
+        }
     }
-}
+    
