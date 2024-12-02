c976d893aa718cdbbe5420e9df56d04286f8644b
Sacred3v <sacredevelopment344@gmail.com>
2024-12-02 14:26:12 -0600

    Parcial03 - Add photo gallery

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,9 +4,10 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideToastr} from 'ngx-toastr';
+import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptors/error.interceptor';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,13 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideToastr({
       positionClass: "toast-bottom-right"
-    })
+    }),
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'contain'
+      } as GalleryConfig
+    }
   ]
 };
