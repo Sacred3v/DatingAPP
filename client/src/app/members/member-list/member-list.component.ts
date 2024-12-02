fdc1755f2bfb94aa56bdc8ae0c9b145cd8c9196b
Sacred3v <sacredevelopment344@gmail.com>
2024-12-02 16:47:14 -0600

    Parcial03 - Add data recovery from app state

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { MemberCardComponent } from '../member-card/member-card.component';
+
 @Component({
   selector: 'app-member-list',
   standalone: true,
@@ -10,16 +11,15 @@ import { MemberCardComponent } from '../member-card/member-card.component';
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
-  private membersService = inject(MembersService);
-  members: Member[] = [];
+  membersService = inject(MembersService);
   
   ngOnInit(): void {
-    this.loadMembers();
+    if (this.membersService.members().length === 0) {
+      this.loadMembers();
+    }
   }
 
   loadMembers() {
-    this.membersService.getMembers().subscribe({
-      next: members => this.members = members
-    })
+    this.membersService.getMembers();
   }
 }
